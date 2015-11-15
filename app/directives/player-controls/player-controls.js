ffc12265d052effca21716a5d351b45c05bf85ec
gijs.sijpesteijn <gijs@jpoint.nl>
2015-11-15 15:16:32 +0100

    Connect with lasforce-rest kotlin implementation

--- app/directives/player-controls/player-controls.js
+++ app/directives/player-controls/player-controls.js
@@ -1,44 +1,51 @@
 'use strict';
 
 app.controller('playerControlsCtrl', function ($scope, $interval, animationPlayer, laser) {
-    $scope.loop = true;
-    $scope.streaming = false;
-    $scope.player = animationPlayer;
-    $scope.laser = laser;
+  $scope.loop = true;
+  $scope.streaming = false;
+  $scope.player = animationPlayer;
+  $scope.laser = laser;
 
-    $scope.toggleLoop = function () {
-        $scope.loop = !$scope.loop;
-        animationPlayer.setLoop($scope.loop);
-    };
+  $scope.toggleLoop = function () {
+    $scope.loop = !$scope.loop;
+    animationPlayer.setLoop($scope.loop);
+  };
 
-    $scope.toggleStreaming = function () {
-        $scope.streaming = !$scope.streaming;
-        laser.setStreaming($scope.streaming);
-    };
+  $scope.toggleStreaming = function () {
+    $scope.streaming = !$scope.streaming;
+    laser.setStreaming($scope.streaming);
+  };
 
-    $scope.$watch('animation.metadata.framerate', function(newVal) {
-        if (angular.isDefined(newVal) && animationPlayer.isPlaying()) {
-            animationPlayer.stop();
-            animationPlayer.play();
-        }
-    }, true);
+  $scope.$watch('animation.metadata.framerate', function (newVal) {
+    if (angular.isDefined(newVal) && animationPlayer.isPlaying()) {
+      animationPlayer.stop();
+      animationPlayer.play();
+    }
+  }, true);
 
-    $scope.$watch('animation', function() {
-        if (angular.isDefined($scope.animation)) {
-            animationPlayer.init($scope.animation);
-        }
-    });
+  $scope.$watch('animation', function () {
+    if (angular.isDefined($scope.animation)) {
+      animationPlayer.init($scope.animation);
+    }
+  });
+
+  $scope.$watch(function() { return animationPlayer.getCurrentFrameId();}, function (currentFrameId) {
+    if ($scope.streaming && angular.isDefined(currentFrameId) && angular.isDefined($scope.animation)) {
+      console.log('CURRENT ID: ' + animationPlayer.getCurrentFrameId());
+      laser.play($scope.animation, currentFrameId);
+    }
+  }, true);
 
 });
 
 app.directive('playerControls', function () {
-    return {
-        templateUrl: 'directives/player-controls/player-controls.html',
-        controller: 'playerControlsCtrl',
-        replace: true,
-        scope: {
-            animation: '=',
-            currentFrameId: '='
-        }
+  return {
+    templateUrl: 'directives/player-controls/player-controls.html',
+    controller: 'playerControlsCtrl',
+    replace: true,
+    scope: {
+      animation: '=',
+      currentFrameId: '='
     }
+  }
 });
--- app/factories/laser.js
+++ app/factories/laser.js
@@ -1,46 +1,59 @@
 'use strict';
 
-app.factory('laser', function(settings) {
-    var streaming = false;
-    var ws;
+app.factory('laser', function (settings, paperWrapper) {
+  var streaming = false;
+  var connected = false;
+  var ws;
 
-    var init = function() {
-        ws = new WebSocket(settings.get('rest.templ.laser'));
-        ws.onmessage = function(event) {
-            console.log('MESSAGE: ' + event.data);
-        };
-        ws.onopen = function(event) {
-            console.log('OPEN');
-        };
-        ws.onclose = function(event) {
-            console.log('CLOSE');
-        };
+  var init = function () {
+    ws = new WebSocket(settings.get('rest.templ.laser'));
+    ws.onmessage = function (event) {
+      console.log('MESSAGE: ' + event.data);
     };
-
-    var isOn = function() {
-      return true;
+    ws.onopen = function (event) {
+      connected = true;
     };
-
-    var isStreaming = function() {
-        return streaming;
+    ws.onclose = function (event) {
+      connected = false;
     };
+  };
 
-    var setStreaming = function(s) {
-        streaming = s;
-    };
+  var play = function(animation, frameId) {
+    var frame = paperWrapper.getPaperObjectMetaData(animation.frames[frameId].paperObject);
+    var mesg = {type:'playFrame',frame:frame};
+    //ws.binaryType = "arraybuffer";
+    //var dataBytes = new Uint8Array(mesg.frameId);
+    //var data = new ArrayBuffer(10000000);
+    //ws.send(dataBytes);
+    ws.send(JSON.stringify(mesg));
+  };
 
-    init();
+  var isOn = function () {
+    return connected;
+  };
 
-    return {
-        isOn: function() {
-            return isOn();
-        },
-        isStreaming: function() {
-            return isStreaming();
-        },
-        setStreaming: function (streaming) {
-            setStreaming(streaming);
-        }
+  var isStreaming = function () {
+    return streaming;
+  };
 
+  var setStreaming = function (s) {
+    streaming = s;
+  };
+
+  init();
+
+  return {
+    isOn: function () {
+      return isOn();
+    },
+    isStreaming: function () {
+      return isStreaming();
+    },
+    setStreaming: function (streaming) {
+      setStreaming(streaming);
+    },
+    play: function (animation, frameId) {
+      play(animation, frameId);
     }
+  }
 });
